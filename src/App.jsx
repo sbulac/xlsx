a25f6a0a9d515107fc736047ac420da3d130f9f0
Sebastian Bula <128990941+sbulac@users.noreply.github.com>
2023-10-24 16:01:01 +0000

    feat: center

--- src/App.jsx
+++ src/App.jsx
@@ -7,31 +7,31 @@ import Header from "./components/Header";
 
 function App() {
   return (
-    <>
+    <Box sx={{ height: "100vh" }}>
       <ApiProvider>
         <ApiContext.Consumer>
           {({ programa, setPrograma, lideres, setLider, nombres, setNombre, zona, mesa, puesto }) => (
             <>
               <Header />
-              <Box sx={{display: "flex"}} >
-              <Form>
-                <ComboBox
-                  setCategory={setPrograma}
-                  label="Programa"
-                  arr={["1", "2", "3", "4", "5", "6"]}
-                />
-                <ComboBox setCategory={setLider} label="Lider" arr={lideres} />
-                <ComboBox setCategory={setNombre} label="Nombre" arr={nombres} />
-                <ComboBoxDisable label="Zona" value={zona} />
-                <ComboBoxDisable label="Mesa" value={mesa} />
-                <ComboBoxDisable label="Puesto" value={puesto} />
-              </Form>
-            </Box>
-        </>
+              <Box sx={{ display: "flex", alignItems: "center", justifyContent: "center", mt: 5 }} >
+                <Form>
+                  <ComboBox
+                    setCategory={setPrograma}
+                    label="Programa"
+                    arr={["1", "2", "3", "4", "5", "6"]}
+                  />
+                  <ComboBox setCategory={setLider} label="Lider" arr={lideres} />
+                  <ComboBox setCategory={setNombre} label="Nombre" arr={nombres} />
+                  <ComboBoxDisable label="Zona" value={zona} />
+                  <ComboBoxDisable label="Mesa" value={mesa} />
+                  <ComboBoxDisable label="Puesto" value={puesto} />
+                </Form>
+              </Box>
+            </>
           )}
-      </ApiContext.Consumer>
-    </ApiProvider >
-    </>
+        </ApiContext.Consumer>
+      </ApiProvider >
+    </Box>
   );
 }
 
